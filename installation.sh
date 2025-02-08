9ecf15355b331e4fb77eb8e745fa212731c4b3e8
Deepesh Gupta <deepeshgupta3257@gmail.com>
2025-02-09 00:07:14 +0530

    Jenkinsfile Added

---
+++ installation.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+sudo apt update -y
+
+# Jenkin's Installation
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+#install docker
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo usermod -aG docker ubuntu  
+newgrp docker
+sudo chmod 777 /var/run/docker.sock
+
+# Sonarqube container
+# docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
+docker run -d --name sonar --restart unless-stopped -p 9000:9000 sonarqube:lts-community
+
+#Trivy Installation
+sudo apt-get install wget apt-transport-https gnupg lsb-release -y
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy -y
